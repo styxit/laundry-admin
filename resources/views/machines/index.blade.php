d2e39c3987e6adf11c09e9c20946339730c74fad
Styxit <info@styxit.com>
2017-04-29 18:13:44 +0200

    Wrap pages in template

--- resources/views/machines/index.blade.php
+++ resources/views/machines/index.blade.php
@@ -1,9 +1,20 @@
-<table>
-@foreach ($machines as $machine)
-    <tr>
-        <td>{{ $machine['id'] }}</td>
-        <td>
-            {{ link_to_action('MachineController@show', $machine['name'], [$machine['id']]) }}
-    </tr>
-@endforeach
-</table>
+@extends('adminlte::page')
+
+@section('title', 'Machines')
+
+@section('content_header')
+    <h1>Machines</h1>
+@stop
+
+@section('content')
+    <p>All machines.</p>
+    <table>
+        @foreach ($machines as $machine)
+            <tr>
+                <td>{{ $machine['id'] }}</td>
+                <td>
+                {{ link_to_action('MachineController@show', $machine['name'], [$machine['id']]) }}
+            </tr>
+        @endforeach
+    </table>
+@stop
--- resources/views/machines/show.blade.php
+++ resources/views/machines/show.blade.php
@@ -1,2 +1,11 @@
-<h1>Machine details</h1>
-<h2>{{ $machine['name'] }}</h2>
+@extends('adminlte::page')
+
+@section('title', 'Machines')
+
+@section('content_header')
+    <h1>Machine details</h1>
+@stop
+
+@section('content')
+    <h2>{{ $machine['name'] }}</h2>
+@stop
