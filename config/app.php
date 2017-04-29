9b3c0f572469c7530838bce1b48fdd78f640bb24
Styxit <info@styxit.com>
2017-04-29 18:07:27 +0200

    Install AdminLte

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
     ],
 
     /*
