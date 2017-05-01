1a8d2bf3755cfec269a081d8b48415985d7b6696
Styxit <info@styxit.com>
2017-05-01 20:18:58 +0200

    Create new machine state
    Not yet linked to a machine

---
+++ app/Http/Controllers/MachineStateController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MachineState;
+use Illuminate\Http\Request;
+
+class MachineStateController extends Controller
+{
+    /**
+     * Store a new machine state.
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        // validate
+        $this->validate(
+            $request,
+            [
+                'seconds_remaining' => 'required',
+            ]
+        );
+
+        // Store machine in database.
+        $state = new MachineState;
+        $state->seconds_remaining = $request->get('seconds_remaining');
+        $state->save();
+
+        return $state->id;
+    }
+}
