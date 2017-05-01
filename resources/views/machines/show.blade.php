539d1a8c8eba3c9e671934f6741db0eb2160b1f8
Styxit <info@styxit.com>
2017-05-01 19:25:30 +0200

    Style machine details

--- resources/views/machines/show.blade.php
+++ resources/views/machines/show.blade.php
@@ -7,7 +7,25 @@
 @stop
 
 @section('content')
-    <h2>{{ $machine['name'] }}</h2>
-    <h2>Brand {{ $machine['brand'] }}</h2>
-    <h2>Model {{ $machine['model'] }}</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <i class="fa fa-cube"></i>
+                    <h3 class="box-title">{{ $machine['name'] }}</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <dl>
+                        <dt>Brand</dt>
+                        <dd>{{ $machine['brand'] }}</dd>
+                        <dt>Model</dt>
+                        <dd>{{ $machine['model'] }}</dd>
+                    </dl>
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+        </div>
+    </div>
 @stop
