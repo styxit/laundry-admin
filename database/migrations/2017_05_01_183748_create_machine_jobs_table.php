093a38d198808ceb4558b53a63013f1fbd15a1c1
Styxit <info@styxit.com>
2017-05-01 20:48:38 +0200

    Create machine jobs table

---
+++ database/migrations/2017_05_01_183748_create_machine_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMachineJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machine_jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('machine_id')->unsigned();
+            $table->unsignedTinyInteger('duration');
+            $table->boolean('completed')->default(false);
+            $table->timestamps();
+
+            // Foreign keys.
+            $table->foreign('machine_id')->references('id')->on('machines');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('machine_jobs');
+    }
+}
