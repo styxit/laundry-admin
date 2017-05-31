c06a551fc72d904d65a1e3b8ccf0e9e845ea6305
Styxit <info@styxit.com>
2017-05-31 23:48:32 +0200

    Initial make:auth

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('machine_jobs/{id}', 'MachineJobController@show')->name('machine_job.
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
