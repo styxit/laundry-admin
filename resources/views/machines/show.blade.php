469d7ade26eb34342d6e6689c78786c0ae66912c
Styxit <info@styxit.com>
2017-12-10 16:57:39 +0100

    Only show link to job if the state has a job

--- resources/views/machines/show.blade.php
+++ resources/views/machines/show.blade.php
@@ -111,7 +111,11 @@
                                     <td>{{ $state->seconds_remaining }}</td>
                                     <td>@duration($state->seconds_remaining)</td>
                                     <td>{{ $state->created_at->format('d-m-Y G:i:s') }}</td>
-                                    <td>{{ link_to_route('machine_job.show', $state->job->id, [$state->job->id]) }}</td>
+                                    <td>
+                                        @if($state->job)
+                                            {{ link_to_route('machine_job.show', $state->job->id, [$state->job->id]) }}
+                                        @endif
+                                    </td>
                                 </tr>
                             @endforeach
                         </table>
