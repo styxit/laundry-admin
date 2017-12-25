319c29c97569ac373e7e5f63097273a3df8d8ae6
Styxit <info@styxit.com>
2017-12-25 15:30:50 +0100

    Create a command to add a new user

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user';
+
+    /**
+     * @var User The User model.
+     */
+    private $user;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param User $user The User model.
+     */
+    public function __construct(User $user)
+    {
+        parent::__construct();
+
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Ask for the new user's name.
+        $name = $this->ask('What is the name of the new user:');
+        // Ask for the new user's email.
+        $email = $this->ask(sprintf('What is the email address of %s?', $name));
+        // Ask for the new user's email.
+        $password = $this->secret(sprintf('Provide a password for %s: (hidden)', $name));
+
+        $user = new $this->user([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        if($user->save()) {
+            $this->output(sprintf('User %s has been created.', $name));
+        } else {
+            $this->output(sprintf('User %s has cloud not be created.', $name));
+        }
+    }
+}
