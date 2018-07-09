4787a2c796412d5242789d0be9abbdd4b8cb750f
Styxit <info@styxit.com>
2018-07-09 21:29:35 +0200

    Schedule job cleanup command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Console\Commands\CreateUser;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,9 +12,7 @@ class Kernel extends ConsoleKernel
      *
      * @var array
      */
-    protected $commands = [
-        CreateUser::class,
-    ];
+    protected $commands = [];
 
     /**
      * Define the application's command schedule.
@@ -25,8 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('jobs:finish')->daily();
     }
 
     /**
