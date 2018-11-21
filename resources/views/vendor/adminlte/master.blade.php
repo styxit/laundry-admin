6f5d3033a3dceae7c19470ade369f361945a40ea
Styxit <info@styxit.com>
2018-11-21 23:01:04 +0100

    Republish adminlte views

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -31,8 +31,8 @@
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/AdminLTE.min.css') }}">
 
     @if(config('adminlte.plugins.datatables'))
-        <!-- DataTables -->
-        <link rel="stylesheet" href="//cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.css">
+        <!-- DataTables with bootstrap 3 style -->
+        <link rel="stylesheet" href="//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.css">
     @endif
 
     @yield('adminlte_css')
@@ -59,8 +59,8 @@
 @endif
 
 @if(config('adminlte.plugins.datatables'))
-    <!-- DataTables -->
-    <script src="//cdn.datatables.net/v/bs/dt-1.10.15/datatables.min.js"></script>
+    <!-- DataTables with bootstrap 3 renderer -->
+    <script src="//cdn.datatables.net/v/bs/dt-1.10.18/datatables.min.js"></script>
 @endif
 
 @if(config('adminlte.plugins.chartjs'))
