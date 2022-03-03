784a0dc5a425177f28524768d192a2bf65e46ab3
Styxit <info@styxit.com>
2022-03-04 00:21:42 +0100

    Disable route with closure

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('machines/{machineId}/machine_states', 'MachineStateController@store')->middleware('machineTokenAuth');
